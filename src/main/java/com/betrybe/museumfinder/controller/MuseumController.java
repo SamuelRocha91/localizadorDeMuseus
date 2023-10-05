a664e9c5811b50335d29994898e8d3c027d392c5
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-05 18:28:30 -0300

    requisito 06

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,16 +1,23 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -41,5 +48,20 @@ public class MuseumController {
         .body(museumService.createMuseum(ModelDtoConverter.dtoToModel(museumCreationDto)));
   }
 
-
+  /**
+   * cria metodo de localizar museus proximos.
+   *
+   * @param lat       double.
+   * @param lng       double.
+   * @param maxDistKm double.
+   * @return o mais proximo.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getClosestMuseum(@RequestParam double lat,
+      double lng, @RequestParam("max_dist_km") double maxDistKm) {
+    Coordinate coordinate = new Coordinate(lat, lng);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(ModelDtoConverter
+            .modelToDto(museumService.getClosestMuseum(coordinate, maxDistKm)));
+  }
 }
