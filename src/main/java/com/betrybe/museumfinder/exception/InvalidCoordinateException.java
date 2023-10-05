0986af3d130da6c3fe7874c682030a6afef54d15
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-05 15:53:30 -0300

    requisito 03 metodo createmuseum da service e excecao unchecked por invalida coordenada

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,9 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * lança uma exceção unchecked por invalida coordenada.
+ *
+ */
+public class InvalidCoordinateException extends RuntimeException{
+
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumServixe.java
@@ -0,0 +1,43 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Cria a camada de Service da aplicação de museus.
+ *
+ */
+@Service
+public class MuseumServixe implements MuseumServiceInterface {
+
+  private MuseumFakeDatabase museumFakeDatabase;
+
+  @Autowired
+  public MuseumServixe(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    boolean isValidCoordinate = CoordinateUtil.isCoordinateValid(museum.getCoordinate());
+    if (!isValidCoordinate) {
+      throw new InvalidCoordinateException();
+    }
+    return museumFakeDatabase.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
